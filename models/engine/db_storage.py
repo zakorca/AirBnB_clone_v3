717bb86122bd24c5a16a1ed57ed296dfbc2ab9ac
ZAKARIA TOURAS <zakariatouras47@gmail.com>
2024-08-12 18:52:19 -0700

    fixed

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,22 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """ Returns the object based on the class and its ID """
+        if cls and id:
+            for obj in self.all(cls).values():
+                if obj.id == id:
+                    return obj
+
+        return None
+
+    def count(self, cls=None):
+        """Returns the number of objs in storage matching the given class"""
+        if cls is None:
+            count = 0
+            for c in classes.values():
+                count += len(self.all(c))
+        else:
+            count = len(self.all(cls))
+        return count
